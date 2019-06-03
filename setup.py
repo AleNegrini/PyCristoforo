73e32706eef01b98f50c6e0fc501ca4b0f066a71
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-06-03 21:57:46 +0200

    First ever commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycristoforo",
+    version="0.0.1",
+    author="Alessandro Negrini",
+    author_email="alessandro2.negrini@gmail.com",
+    description="Random latitude and longitude generator by country",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
