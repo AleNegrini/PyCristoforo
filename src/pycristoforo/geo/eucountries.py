e7b6a34d06a4b4ca05c70156a294091fc79a6e3d
Alessandro Negrini <alessandro2.negrini@gmail.com>
2019-06-23 17:58:11 +0200

    Added documentation to methods

--- src/pycristoforo/geo/eucountries.py
+++ src/pycristoforo/geo/eucountries.py
@@ -6,7 +6,7 @@ class EUCountryList:
 
     def __init__(self, full_path):
         """
-        Inizializer method that builds the country dictionary with 'key','value' pair
+        Constructor method that builds the country dictionary with 'key','value' pair
         :param full_path: path where the geojson is stored
         """
         self.__country_dict = {}
@@ -24,6 +24,11 @@ class EUCountryList:
             self.__country_dict[country.key] = country.value
 
     def get_by_key(self, key: str) -> int:
+        """
+        It returns the element with a given key of the private dictionary
+        :param key: dict key
+        :return: dict element
+        """
         try:
             return self.__country_dict[key]
         except KeyError as ex:
@@ -31,4 +36,8 @@ class EUCountryList:
             return None
 
     def get_country_dict(self):
+        """
+        Getter method for private attribute "country_dict"
+        :return: dictionary of countries
+        """
         return self.__country_dict
